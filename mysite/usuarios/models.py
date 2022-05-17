08540acce727bdf616ef835b8794734e5b22e900
Fábio <fabio@fjss-Aspire-5750Z>
2022-05-17 19:04:15 -0300

    Correção bug banco de dados

--- mysite/usuarios/models.py
+++ mysite/usuarios/models.py
@@ -4,5 +4,5 @@ class Usuario(models.Model):
     nome = models.CharField(max_length=30)
     sobrenome = models.CharField(max_length=45)
     email = models.EmailField(max_length=30)
-    senha = models.CharField(max_length=64)
+    senha = models.CharField(max_length=64, null=False)
 
--- mysite/usuarios/views.py
+++ mysite/usuarios/views.py
@@ -20,21 +20,6 @@ def valida_cadastro(request):
     email = request.POST.get('email')
     senha = request.POST.get('celular')
     
-    Usuario = Usuario.objects.filter(email = email)
-
-    if len(nome.strip()) == 0 or len(email.strip()) == 0:
-        return redirect('auth/cadastro/?status=1')
-
-    if len(senha) < 8:
-        return redirect('auth/cadastro/?status=2')
-
-    if len(usuario) > 0 :
-            return redirect('/auth/cadastro/?status=3')
-    try:
-        senha = sha256(senha.encode()).hexdigest()
-        usuario = Usuario(nome = nome, sobrenome = sobrenome, email = email, senha = senha)
-        usuario.save()
-        return redirect('/auth/cadastro/?status=0')    
-    except:
-        return redirect('auth/cadastro/?status=4')    
-    return HttpResponse(f"{nome} {senha} {email}")
+    usuario = Usuario(nome=nome, sobrenome=sobrenome, email=email, senha=senha)
+    usuario.save()
+    
