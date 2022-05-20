79553caeb75472c5d4c56600df88581819be8fd5
Fábio <fabio@fjss-Aspire-5750Z>
2022-05-20 01:42:36 -0300

    banco de dados cursos/template

--- mysite/cursos/views.py
+++ mysite/cursos/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import CursosDisponiveis
 # Create your views here.
+def cursos(request):
+    cursos = CursosDisponiveis.objects.all()
+    return render(request, 'templates/cursos.html', {'cursos': cursos})
 
 def home(request):
-    return render(request, 'mysite/index.html')
+    return render(request, 'cursos.html')
